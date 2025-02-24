193a68789b318fe23b29fe0c499517314dfec800
RolandSall <roland.salloum00@outlook.com>
2025-02-24 09:28:45 +0200

    feat: writing aa dummy task

--- application-example/build.gradle.kts
+++ application-example/build.gradle.kts
@@ -1,14 +1,25 @@
 plugins {
     id("myproject.spring-conventions")
     id("myproject.testing-conventions")
+    id("myproject.package-conventions")
+    id("application")
 }
 
-group = "com.rolandsall"
+
+tasks.generateScript {
+    mainClass.set("com.rolandsall.Main")
+}
+
+myApp {
+
+}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation ("org.springframework.boot:spring-boot-starter-test")
 }
+
+
 
 
 
